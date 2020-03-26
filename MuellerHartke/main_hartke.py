c11e5f3aec7e17368ffd75776a3f0043a97a7542
Leo Komissarov <komissarov@scm.com>
2020-03-26 19:36:50 +0100

    See last commit

--- MuellerHartke/main_hartke.py
+++ MuellerHartke/main_hartke.py
@@ -1,15 +1,13 @@
-import os
+#!/usr/bin/env python
+import os, sys
 from os.path import join as opj
 import numpy as np
 
 from scm.params import *
 from scm.plams import Settings
 
-INDIR = 'input_params'
-
-### STEP 1: Convert old reax formats to params
-if not os.path.exists(INDIR):
-    os.makedirs(INDIR)
+from scm.params.common.helpers import _cleanup_example_folder
+_cleanup_example_folder(['opt*'])
 
 def add_grads(path, dataset):
     factor = 1. #Units.convert(1.0, 'au', 'kcal/mol') * Units.convert(1.0, 'bohr^-1', 'angstrom^-1')
@@ -23,6 +21,13 @@ def add_grads(path, dataset):
                     dataset.add_entry(f'force("{name}", {id}, {xyz})', weight=0.01, reference=-value)
 
 
+INDIR = 'input_params'
+if not os.path.exists(INDIR):
+    os.makedirs(INDIR)
+
+
+
+### STEP 1: Convert old reax formats to params
 print('Converting geo files ...')
 s = Settings()
 s.input.ams.properties.gradients = True # We need to specifically request gradients with AMS
@@ -31,16 +36,21 @@ jc2 = geo_to_params('SI/valSet/geo',   normal_run='GO', settings=s)
 print('The following jobIDs are in *both* the training and validation sets:')
 print("\n".join([i for i in jc1.keys() if i in jc2])+'\n')
 jc  = jc1 + jc2
+for e in jc.values():
+    e.metadata['Source'] = 'https://doi.org/10.1021/acs.jctc.6b00461'
 jc.store(opj(INDIR, 'jobcollection.yml'))
 
 print('Converting trainset.in files ...')
 train_set = trainset_to_params('SI/optInput/trainset.in')
 add_grads('SI/optInput/grads', train_set)
 val_set  =  trainset_to_params('SI/valSet/trainset.in')
+for e in train_set+val_set:
+    e.metadata['Source'] = 'https://doi.org/10.1021/acs.jctc.6b00461'
 train_set.store(opj(INDIR, 'trainingset.yml'))
 val_set.store(  opj(INDIR, 'validationset.yml'))
 
 print(f"Params converted sets stored to .{os.sep+INDIR}")
+
 
 
 ### STEP 2: Calculate the fx value of x0 and x*
@@ -62,16 +72,20 @@ print('Published training set value is 16271 (doi.org/10.1021/acs.jctc.9b00769)'
 
 
 ### STEP 3: Set up an optimization
-x = xopt # Select which ffield to optimize here
+x = xopt # Select which ffield to optimize here: xopt, x0
 
 # Use this mask to select the .active subset of parameters for the optimization
 is_active_mask = [x0[i] != xopt[i] for i in x0.names] # All values that differ between two ffields will be optimized
 x.is_active = is_active_mask
 
-print(f"\nWill optimize (len.xopt
+badids = [i for i,e in jc.items() if not e.is_pipeable()]
+assert badids == [], 'Some jobs are not pipeable!'
+
+print('\nNow optimizing:\n')
 o = CMAOptimizer(popsize=10, sigma=0.25)
-callbacks = [Logger(plot=True), Timeout(60*60), TimePerEval(5), EarlyStopping(1000)]
-optimization = Optimization(jc, [train_set, val_set], xopt, o, callbacks=callbacks)
+
+callbacks = [Logger(plot=False), Timeout(5*60), TimePerEval(5), EarlyStopping(1000)]
+optimization = Optimization(jc, [train_set, val_set], x, o, callbacks=callbacks)
 
 optimization.summary()
 optimization.optimize()
