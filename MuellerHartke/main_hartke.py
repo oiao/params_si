c480f00339a8dd8f4bd1f507f84a6eb6caccabe8
Leo Komissarov <komissarov@scm.com>
2020-04-06 11:55:07 +0200

    Add constraints

--- MuellerHartke/main_hartke.py
+++ MuellerHartke/main_hartke.py
@@ -4,7 +4,6 @@ from os.path import join as opj
 import numpy as np
 
 from scm.params import *
-from scm.plams import Settings
 
 from scm.params.common.helpers import _cleanup_example_folder
 _cleanup_example_folder(['opt*'])
@@ -20,6 +19,24 @@ def add_grads(path, dataset):
                 for xyz,value in enumerate(at):
                     dataset.add_entry(f'force("{name}", {id}, {xyz})', weight=0.01, reference=-value)
 
+def get_constraints(x):
+    from itertools import combinations
+    pre_atoms = ['C:', 'H:', 'O:', 'S:']
+    pre_offd = [f"{'.'.join(i)}:" for i in combinations(['C', 'H', 'O', 'S'], 2)]
+    const = []
+    for pre in pre_atoms:
+        sigma = f"{pre}r_0^sigma;;2;;Sigma bond covalent radius"
+        pi    = f"{pre}r_0^pi;;2;;Pi bond covalent radius"
+        pi2   = f"{pre}r_0^pi;;2;;Double pi bond covalent radius"
+        const += [x[sigma] >= x[pi], x[pi] >= x[pi2]]
+    for pre in pre_offd:
+        sigma = f"{pre}r_0^sigma;;2;;Sigma bond length"
+        pi    = f"{pre}r_0^pi;;2;;Pi bond length"
+        pi2   = f"{pre}r_0^pipi;;2;;PiPi bond length"
+        const += [x[sigma] >= x[pi], x[pi] >= x[pi2]]
+    return const
+
+
 
 INDIR = 'input_params'
 if not os.path.exists(INDIR):
@@ -29,15 +46,15 @@ if not os.path.exists(INDIR):
 
 ### STEP 1: Convert old reax formats to params
 print('Converting geo files ...')
-s = Settings()
-s.input.ams.properties.gradients = True # We need to specifically request gradients with AMS
-jc1 = geo_to_params('SI/optInput/geo', normal_run='GO', settings=s)
-jc2 = geo_to_params('SI/valSet/geo',   normal_run='GO', settings=s)
+jc1 = geo_to_params('SI/optInput/geo', normal_run_settings='SI/control')
+jc2 = geo_to_params('SI/valSet/geo',   normal_run_settings='SI/control')
 print('The following jobIDs are in *both* the training and validation sets:')
 print("\n".join([i for i in jc1.keys() if i in jc2])+'\n')
+
 jc  = jc1 + jc2
 for e in jc.values():
     e.metadata['Source'] = 'https://doi.org/10.1021/acs.jctc.6b00461'
+    e.settings.input.ams.properties.gradients = True # We need to specifically request gradients with AMS
 jc.store(opj(INDIR, 'jobcollection.yml'))
 
 print('Converting trainset.in files ...')
@@ -54,8 +71,8 @@ print(f"Params converted sets stored to .{os.sep+INDIR}")
 
 
 ### STEP 2: Calculate the fx value of x0 and x*
-x0   = ReaxParams('SI/ffield_mattson_modified')
-xopt = ReaxParams('SI/ffield_best')
+x0   = ReaxParams('SI/ffield_mattson_modified', 1.15)
+xopt = ReaxParams('SI/ffield_best', 1.15)
 
 print()
 for i in [x0, xopt]: # Do the same twice
@@ -78,14 +95,17 @@ x = xopt # Select which ffield to optimize here: xopt, x0
 is_active_mask = [x0[i] != xopt[i] for i in x0.names] # All values that differ between two ffields will be optimized
 x.is_active = is_active_mask
 
+# All jobs should run through the pipe
 badids = [i for i,e in jc.items() if not e.is_pipeable()]
 assert badids == [], 'Some jobs are not pipeable!'
+
 
 print('\nNow optimizing:\n')
 o = CMAOptimizer(popsize=10, sigma=0.25)
 
-callbacks = [Logger(plot=False), Timeout(5*60), TimePerEval(5), EarlyStopping(1000)]
-optimization = Optimization(jc, [train_set, val_set], x, o, callbacks=callbacks)
+callbacks = [Logger(plot=False), Timeout(60*60*2), TimePerEval(10), EarlyStopping(1000)]
+constraints = get_constraints(x)
+optimization = Optimization(jc, [train_set, val_set], x, o, callbacks=callbacks, constraints=constraints)
 
 optimization.summary()
 optimization.optimize()
